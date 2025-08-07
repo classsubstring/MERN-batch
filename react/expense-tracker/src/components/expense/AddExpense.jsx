0fd24143c5061a99c48277bf1b05bf9d4b2e19a3
batchlcwd <batchlcwd@gmail.com>
2025-08-07 12:52:59 +0530

    validation added in expense form

--- react/expense-tracker/src/components/expense/AddExpense.jsx
+++ react/expense-tracker/src/components/expense/AddExpense.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
+import { RxCross2 } from "react-icons/rx";
 
 function AddExpense() {
   const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("Write about..");
+  const [description, setDescription] = useState("");
   const [expDate, setExpDate] = useState("");
   const [category, setCategory] = useState("recharge");
+  const [error, setError] = useState({
+    title: "",
+    description: "",
+  });
 
   const [expenses, setExpenses] = useState([]);
 
@@ -27,6 +32,41 @@ function AddExpense() {
 
   function handleForm(event) {
     event.preventDefault();
+
+    let hasError = false;
+
+    const localError = {
+      title: "",
+      description: "",
+    };
+
+    // title validattion
+    if (title.trim() == "") {
+      console.log("title is blank");
+
+      localError.title = "Expenes Title is required !!";
+
+      hasError = true;
+    } else if (title.length <= 5) {
+      localError.title = "In Title min 6 characters are required !!";
+
+      hasError = true;
+    }
+
+    // content
+    if (description.trim() == "") {
+      console.log("desc is blank");
+      localError.description = "Write something about your expense !!";
+
+      hasError = true;
+    }
+
+    if (hasError) {
+      console.log("form has error");
+      setError(localError);
+      return;
+    }
+
     console.log("form submit");
     const expese = {
       title,
@@ -36,23 +76,32 @@ function AddExpense() {
     };
     console.log(expese);
     setExpenses([...expenses, expese]);
+    setError({
+      title: "",
+      description: "",
+    });
   }
   return (
     <div className="m-4 flex gap-3  justify-center">
       {/* show all expense */}
       <div className="w-full md:w-1/2">
-        {expenses.length > 0 && (
+        {expenses.length > 0 ? (
           <h1 className="text-3xl font-bold my-3 ">
             Expense List : {expenses.length}
           </h1>
+        ) : (
+          <h1 className="text-3xl font-bold">No Expense Available</h1>
         )}
 
         <div className="expense_container">
           {expenses.map((exp, index) => {
             return (
-              <div className="p-3 bg-slate-300 rounded-xl m-1">
+              <div className="p-3 relative bg-slate-300 rounded-xl m-1">
                 <h1>{exp.title}</h1>
                 <p>{exp.description}</p>
+                <button className="absolute top-4 right-4 p-3 hover:bg-slate-700 hover:text-white rounded-full cursor-pointer transition-all duration-100">
+                  <RxCross2 />
+                </button>
               </div>
             );
           })}
@@ -63,6 +112,12 @@ function AddExpense() {
         onSubmit={handleForm}
         className="  w-full md:w-1/2 p-10 rounded-2xl bg-slate-300"
       >
+        {/* Error */}
+        {/* {error && (
+          <div className="error_container p-3 bg-red-500 text-white rounded my-3">
+            <p>{error}</p>
+          </div>
+        )} */}
         {/* title field */}
 
         <div className="flex gap-2 mb-3 flex-col">
@@ -76,6 +131,8 @@ function AddExpense() {
             value={title}
             placeholder="Enter title here"
           />
+
+          {error.title && <p className="text-red-600 ml-2">{error.title}</p>}
         </div>
 
         {/* description field */}
@@ -92,6 +149,9 @@ function AddExpense() {
             value={description}
             placeholder="Enter title here"
           ></textarea>
+          {error.description && (
+            <p className="text-red-600 ml-2">{error.description}</p>
+          )}
         </div>
 
         {/* date field */}
@@ -130,7 +190,20 @@ function AddExpense() {
           >
             Add Expense
           </button>
-          <button className="px-3 py-1 bg-red-700 text-white rounded">
+          <button
+            type="button"
+            onClick={() => {
+              setTitle("");
+              setDescription("");
+              setExpDate("");
+              setCategory("food");
+              setError({
+                title: "",
+                description: "",
+              });
+            }}
+            className="px-3 py-1 bg-red-700 text-white rounded"
+          >
             Clear
           </button>
         </div>
